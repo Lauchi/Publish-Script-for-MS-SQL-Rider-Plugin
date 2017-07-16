373c78cc3d6fc7b44052bc69d1f0600e049c9ffd
mr-modmoto <mr-modmoto@713411d8-43b3-45dd-ae96-fd9f6efd0bf7>
2017-07-16 09:55:37 +0000

    first commit with working sample menu
    git-svn-id: https://svn.riouxsvn.com/publishscriptpl@2 713411d8-43b3-45dd-ae96-fd9f6efd0bf7

---
+++ trunk/PublishToSqlServer/src/MyShowDialog.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by simonheiss on 16.07.17.
+ */
+public class MyShowDialog extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+
+    }
+}
